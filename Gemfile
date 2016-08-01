8450dfcd899e6a708e4dcaa48c55d414a57207c5
Jessica Cotzin <jcotzin@gmail.com>
2016-08-01 19:35:10 -0400

    added HTTparty gem and incorporated openweather api

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'httparty'
 gem 'algoliasearch-rails'
 gem 'hirb'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,16 @@
+require 'httparty'
 class PlacesController < ApplicationController
   def index
+    #using openweather api
+  @today = Date.today
+  @response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=miami&APPID=612104db0b85a84d80dd7cd48d5516c9')
+  @parsed = @response.parsed_response
+  @description = @response.parsed_response["weather"][0]["description"]
+  @temp = @parsed["main"]["temp"]
+  @fahrenheit = @temp * 1.8
+  @degrees = (@fahrenheit - 459.67).round
+
+  #clear sky, few clouds, scattered clouds, broken clouds, shower rain, rain, thunderstorm, snow, mist
+  
   end
 end
