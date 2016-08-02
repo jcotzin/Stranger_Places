3bc44805fb8f6f060fe4503367e8df07e3e2cb3f
David Gonzalez <davidigonzalez1@gmail.com>
2016-08-02 13:20:22 -0400

    added correct js indext name

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,6 +1,6 @@
 //not sure if this goes here. I created this file. Also check if app id and key works. Their values are set in config/initializers/algoriasearch.rb
 var client = algoliasearch(ApplicationID, Search-Only-API-Key);
-var index = client.initIndex('strangerplaces');
+var index = client.initIndex('Place');
 index.search('something', { hitsPerPage: 10, page: 0 })
   .then(function searchDone(content) {
     console.log(content)
